def8356e01891eae2a6683dcf3c14ced1e4efd78
user <jeffdplante@aol.com>
2022-11-27 10:11:03 -0800

    Implement addBookToTable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-let myLibrary = [];
+const myLibrary = [];
+const bookTable = document.getElementById('book-table');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -16,8 +17,36 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function addBookToTable(book) {
+ let row = document.createElement('tr');
+
+ row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.pages}</td>
+  <td>${book.read}</td>
+  <td><a href="#" class=""></a>X</td>
+  <td><a href="#" class="delete"></a>X</td>
+ `;
+
+ bookTable.appendChild(row);
+}
 
 myLibrary.push(new Book('Dune', 'Frank Herbert', 400, true));
 myLibrary.push(new Book('Breathe', 'James Nestor', 200, false));
 
 console.log(myLibrary);
+
+// UI.prototype.addBookToList = function(book){
+//   const list = document.getElementById('book-list');
+//   // create tr element
+//   const row = document.createElement('tr');
+//   row.innerHTML = `
+//     <td>${book.title}</td>
+//     <td>${book.author}</td>
+//     <td>${book.isbn}</td>
+//     <td><a href="#" class="delete"></a>X</td>
+//   `;
+
+//   list.appendChild(row);
+// }
